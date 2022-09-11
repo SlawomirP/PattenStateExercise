7fc050f3746e34d28661f191bd4e1c309b66dc0b
slawo <slawekpettke@gmail.com>
2022-09-11 19:56:31 +0200

    patternStateGame start

--- src/main/java/gameMachine/states/CoinInsertState.java
+++ src/main/java/gameMachine/states/CoinInsertState.java
@@ -5,21 +5,26 @@ import gameMachine.OneHandBandit;
 public class CoinInsertState implements State{
     @Override
     public void insertTheCoin(OneHandBandit oneHandBandit) {
-        System.out.println("you insert the coin");
+        System.out.println("Coin is inside");
     }
 
     @Override
     public void pullTheLever(OneHandBandit oneHandBandit) {
-
+        System.out.println("Drawing...");
     }
 
     @Override
     public void takeThePrize(OneHandBandit oneHandBandit) {
-
+        System.out.println("First pull the lever.");
     }
 
     @Override
     public void printActualStatus(OneHandBandit oneHandBandit) {
+        oneHandBandit.getState();
+    }
 
+    @Override
+    public String toString() {
+        return "Coin inside.";
     }
 }
